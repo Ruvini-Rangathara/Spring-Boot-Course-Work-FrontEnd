db2745a7388ead704c88ad49bf8763959ff7b3fd
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-10-18 19:41:37 +0530

    Add Login/Signup page

---
+++ assert/javaScript/login.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+document.getElementById("popupButton").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "block";
+});
+
+document.getElementById("closeButton").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "none";
+});
+
+// Close the pop-up if the user clicks outside the pop-up
+window.onclick = function(event) {
+    var popup = document.getElementById("popup");
+    if (event.target === popup) {
+        popup.style.display = "none";
+    }
+};
