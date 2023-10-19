4f3014e46f7b184596b985d5f186392b5f5c8d6b
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-10-19 13:08:16 +0530

    Add guide manage form

--- assert/javaScript/login.js
+++ assert/javaScript/login.js
@@ -1,21 +1,25 @@
-
-
-
-
-
-
-document.getElementById("popupButton").addEventListener("click", function() {
+document.getElementById("next-button").addEventListener("click", function () {
     document.getElementById("popup").style.display = "block";
 });
 
-document.getElementById("closeButton").addEventListener("click", function() {
+document.getElementById("closePopup").addEventListener("click", function () {
     document.getElementById("popup").style.display = "none";
 });
 
-// Close the pop-up if the user clicks outside the pop-up
-window.onclick = function(event) {
-    var popup = document.getElementById("popup");
-    if (event.target === popup) {
-        popup.style.display = "none";
-    }
-};
+
+//
+// document.getElementById("popupButton").addEventListener("click", function() {
+//     document.getElementById("popup").style.display = "block";
+// });
+//
+// document.getElementById("closeButton").addEventListener("click", function() {
+//     document.getElementById("popup").style.display = "none";
+// });
+//
+// // Close the pop-up if the user clicks outside the pop-up
+// window.onclick = function(event) {
+//     var popup = document.getElementById("popup");
+//     if (event.target === popup) {
+//         popup.style.display = "none";
+//     }
+// };
